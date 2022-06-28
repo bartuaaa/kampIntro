9230bd252e575b42762c20d72b3bd9aaeba421e3
bartuaaa <bartuaaa@gmail.com>
2022-06-28 20:59:00 +0300

    20 ocak Ders sonu

---
+++ ClassMetotDemo1/MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo1
+{
+    internal class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi");
+        }
+        public void Musterisil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi");
+        }
+        public void MusteriListele(Musteri musteri, Musteri musteri3)
+        {
+            Console.WriteLine(musteri.Id);
+            Console.WriteLine(musteri.Ad);
+            Console.WriteLine(musteri.Soyad);
+            Console.WriteLine(musteri.Yas);
+
+        }
+        
+        
+        
+    }
+    
+}
---
+++ ClassMetotDemo1/Program.cs
@@ -0,0 +1,33 @@
+﻿using ClassMetotDemo1;
+
+Musteri musteri1 = new Musteri();
+musteri1.Id = 12;
+musteri1.Ad = "bartu";
+musteri1.Soyad = "odabaşı";
+musteri1.Yas = "15";
+
+Musteri musteri2 = new Musteri();
+musteri2.Id = 13;
+musteri2.Ad = "emre";
+musteri2.Soyad = "yıldırım";
+musteri2.Yas = "15";
+
+ Musteri musteri3 = new Musteri();
+musteri3.Id = 12;
+musteri3.Ad = "selim";
+musteri3.Soyad = "yonbak";
+musteri3.Yas = "15";
+
+Musteri[] musteri = new Musteri[] {musteri1,musteri2,musteri3};
+
+foreach (var Musteri in musteri)
+{
+    Console.WriteLine(Musteri.Id);
+    Console.WriteLine(Musteri.Ad);
+    Console.WriteLine(Musteri.Soyad);
+    Console.WriteLine(Musteri.Yas);
+}
+MusteriManager musteriManager = new MusteriManager();
+musteriManager.MusteriEkle(musteri3);
+musteriManager.Musterisil(musteri2);
+musteriManager.MusteriListele(musteri2,musteri3);
